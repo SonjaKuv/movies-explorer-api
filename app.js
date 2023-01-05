3223047ca8418636e5c5d5edf818d152a52edfda
SonjaKuv <kuvshinova.sonja@yandex.ru>
2023-01-05 12:43:25 +0300

    fixes before deploy

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
 const handleErrors = require('./middlewares/handleErorrs');
@@ -8,6 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
