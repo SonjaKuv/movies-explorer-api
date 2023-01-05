b831d5e320a03fcaf449b5f4a4d054f9af045169
SonjaKuv <kuvshinova.sonja@yandex.ru>
2023-01-05 14:24:29 +0300

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/index');
 const handleErrors = require('./middlewares/handleErorrs');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -10,6 +11,20 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'http://sonja-diplom.nomoredomains.club',
+    'https://sonja-diplom.nomoredomains.club',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
